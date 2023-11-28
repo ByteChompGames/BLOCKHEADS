4e20ad42b4fde7975715fd523d9a6d4ebffd501d
Jordan <142346296+ByteChompGames@users.noreply.github.com>
2023-11-28 14:39:54 -0600

    Running & Level Bounds
    - run animations
    - run and walk speeds
    - run while chase / flee
    - grass and dirt terrain
    - stay inside terrain bounds
    - path away from bounds when moving

--- ai/ai_actor.gd
+++ ai/ai_actor.gd
@@ -10,11 +10,14 @@ enum States
 }
 var state = States.WAIT
 
-@export var move_speed = 25
+@export var walk_speed = 25
+@export var run_speed = 50
+@export var wander_min_max = Vector2(1, 2)
 @export var on_detect_state = States.CHASE
 @export var detect_state_time = 3
 
 var move_direction : Vector2 = Vector2.ZERO
+var move_speed = 0
 var detect_target = null
 
 @onready var move_timer = $WanderTimer
@@ -33,11 +36,11 @@ func _physics_process(delta):
 		States.WAIT:
 			move_direction = Vector2.ZERO
 		States.WANDER:
-			pass
+			move_speed = walk_speed
 		States.CHASE:
-			move_direction = get_direction_to_target()
+			move_speed = run_speed
 		States.FLEE:
-			move_direction = -get_direction_to_target()
+			move_speed = run_speed
 	
 	velocity = move_direction * move_speed
 	move_and_slide()
@@ -55,14 +58,27 @@ func get_direction_to_target() -> Vector2:
 		var direction = detect_target.global_position - global_position
 		return direction.normalized()
 
+func assign_detect_target(body):
+	detect_target = body
+	state = on_detect_state
+	move_direction = get_direction_to_target()
+	if on_detect_state == States.FLEE:
+		move_direction = -move_direction
+	
+	animated_sprite.play("run")
+	if not wait_timer.is_stopped():
+		wait_timer.stop()
+	if not move_timer.is_stopped():
+		move_timer.stop()
+
 func _on_move_timer_timeout():
 	wait_timer.start()
 	state = States.WAIT
 	animated_sprite.play("idle")
 
-
 func _on_wait_timer_timeout():
 	move_direction = get_random_direction()
+	move_timer.wait_time = randf_range(wander_min_max.x, wander_min_max.y)
 	move_timer.start()
 	state = States.WANDER
 	animated_sprite.play("walk")
@@ -72,17 +88,13 @@ func _on_detection_range_body_entered(body):
 	if body == self:
 		return
 	
-	if detect_target == null:
-		detect_target = body
-		state = on_detect_state
-		animated_sprite.play("walk")
-		if not wait_timer.is_stopped():
-			wait_timer.stop()
-		if not move_timer.is_stopped():
-			move_timer.stop()
-	else:
-		if body == detect_target:
-			detect_timer.stop()
+	if body.is_in_group("actor"):
+		if detect_target == null:
+			assign_detect_target(body)
+		else:
+			if body == detect_target:
+				detect_timer.stop()
+				get_direction_to_target()
 
 
 func _on_detection_range_body_exited(body):
@@ -97,3 +109,9 @@ func _on_detect_timer_timeout():
 	state = States.WAIT
 	wait_timer.start()
 	animated_sprite.play("idle")
+
+
+func _on_detection_bounds_body_entered(body):
+	if body.is_in_group("terrain"):
+		var direction = body.global_position - global_position
+		move_direction = -direction.normalized()
